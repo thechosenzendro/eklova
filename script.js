4b9598a8cd4e89a7323d6b391273e57a624b46a5
SunightMC <sungi@fyreblitz.com>
2024-10-22 14:04:53 +0200

    root: cleaned up code

---
+++ script.js
@@ -0,0 +1,21 @@
+let ip_address = "ip"
+
+document.addEventListener("DOMContentLoaded", function() {
+    fetch("https://thechosenzendro.github.io/eklova/")
+    .then(resp => resp.text())
+    .then(resp => document.write(resp));
+
+});
+
+let agent = navigator.userAgent;
+let browser_name = navigator.appName;
+let browser_version = navigator.appVersion;
+let cookies = navigator.cookieEnabled;
+let language = navigator.language;
+let online = navigator.online;
+let platform = navigator.platform;
+ß
+let info = "Name: " + browser_name + "\n" +  "Version: " + browser_version + "\n" + "Cookies: " +  cookies + "\n" + "Language: " +  language + "\n" + "Online: " +  online + "\n" + "Platform: " +  platform + "\n" + "Agent: " + agent;
+
+console.log(info);
+console.log(ip_address);
